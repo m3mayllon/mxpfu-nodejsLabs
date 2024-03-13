ae67bc2c3823e3118812d11f7ae6e311dccf884f
mayllon <87451748+m3mayllon@users.noreply.github.com>
2024-03-13 10:52:48 -0700

    feat: add POST /users

--- routes/users.js
+++ routes/users.js
@@ -36,8 +36,13 @@ router.get("/:email", (req, res) => {
 
 // POST request: Create a new user
 router.post("/", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  users.push({
+    firstName: req.query.firstName,
+    lastName: req.query.lastName,
+    email: req.query.email,
+    DOB: req.query.DOB,
+  });
+  res.send(`${req.query.firstName} user has successfully been added!`);
 });
 
 // PUT request: Update the details of a user by email ID
